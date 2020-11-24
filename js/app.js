8b1136671237032cf477b7cf2edc705f9e647242
tomekts <tomek_kasperczyk@interia.pl>
2020-11-24 14:07:45 +0100

    ramka menu

--- js/app.js
+++ js/app.js
@@ -24,16 +24,35 @@ document.addEventListener('DOMContentLoaded', function() {
 		buttons[i].addEventListener('click', buttonClick )
 	}   
 	
+	
 	function buttonClick(){
 		console.log("test button click")
 		var textArea =this.previousElementSibling;  
+
+
        	if (textArea.style.display ==="none" || textArea.style.display ==="") {
        		textArea.style.display ="block"
        		this.innerHTML='mniej <span class="glyphicon glyphicon-chevron-up"></span>';
        	}else{
        	    textArea.style.display ="none";      	
-       		this.innerHTML='wiecej <span class="glyphicon glyphicon-chevron-up"></span>';
+       		this.innerHTML='wiecej <span class="glyphicon glyphicon-chevron-down"></span>';
     	}  
 	}
+
+	var cos = document.querySelectorAll('.col-sm-4');
+	console.log(cos[1]);
+
+	 window.addEventListener('scroll', function() {
+      console.log('działa przy scrollu');
+      var bar =document.querySelector('.navbar');
+      bar.style.borderBottom = '1px solid #7f7f7f';
+      
+      var inside =document.querySelector('.dropdown');
+      inside.style.border = '1px solid #7f7f7f';
+      
+
+
+      
+    });
 	
 });
