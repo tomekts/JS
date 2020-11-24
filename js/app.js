a12573e0d1148715456b16ceb87e1a04b8384ede
tomekts <tomek_kasperczyk@interia.pl>
2020-11-24 12:37:37 +0100

    rozwijanie listy

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 	// tutaj będziemy umieszczać kod ze wszystkich zadań
 	var listMenu  = document.querySelector('.for-dropdown');  
-
+	var buttons = document.querySelectorAll('.read-more');
+    
+	
 	listMenu.addEventListener('mouseover', function(){
 		console.log('wjezda na menu');
 		document.querySelector('.dropdown').style.display = 'block';
@@ -13,4 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log('wyjezdza z menu');
 		document.querySelector('.dropdown').style.display = 'none';
 	});
+
+	// buttons[0].addEventListener('click', buttonClick );
+	// buttons[1].addEventListener('click', buttonClick );
+	// buttons[2].addEventListener('click', buttonClick );
+
+	for (var i = 0 ; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', buttonClick )
+	}   
+	
+	function buttonClick(){
+		console.log("test button click")
+		var textArea =this.previousElementSibling;  
+       	if (textArea.style.display ==="none" || textArea.style.display ==="") {
+       		textArea.style.display ="block"
+       		this.innerHTML='mniej <span class="glyphicon glyphicon-chevron-up"></span>';
+       	}else{
+       	    textArea.style.display ="none";      	
+       		this.innerHTML='wiecej <span class="glyphicon glyphicon-chevron-up"></span>';
+    	}  
+	}
+	
 });
